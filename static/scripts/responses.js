fd9877848d70383c2768bcdb32463a74e85edd6c
sohailtiwari <115350644+sohailtiwari@users.noreply.github.com>
2023-02-26 12:46:51 +0530

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,21 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else if (input == "how are you"){
+        return "I am fine what about you";
+    }    else{
+        return "Try asking something else!";
+    }
+}
